32de5c77cdc9fd549307bddf12cec8729a454d4e
atomsmex <atomrimamumar1@gmail.com>
2026-05-05 12:34:26 +0100

    Create main.dart file for Flutter app initialization and theme configuration

---
+++ frontend/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meal Planning App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meal Planning Home Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Meal Planning App!'),
+      ),
+    );
+  }
+}
